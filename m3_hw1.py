df09c7078ef7147de970e1f6c27148824bc50bb3
Faoeltiarna <moriartystreet@gmail.com>
2024-07-01 16:57:24 +0300

    commit_7

--- m3_hw1.py
+++ m3_hw1.py
@@ -1,10 +1,13 @@
 from datetime import datetime
 
+date_value = input("Enter your date in format 'YYYY-mm-dd': ")
 def get_days_from_today(date):
-    date_value = datetime.strptime(date, "%Y-%m-%d").date()
-    current_date = datetime.today().date()
-    difference = date_value.toordinal() - current_date.toordinal()
-    return difference
+    try:
+        date_value = datetime.strptime(date, "%Y-%m-%d").date()
+        current_date = datetime.today().date()
+        difference = date_value.toordinal() - current_date.toordinal()
+        return difference
+    except ValueError:
+        return "Error date fotmat!"
 
-
-print(get_days_from_today("2005-09-01"))
+print(get_days_from_today(date_value))
