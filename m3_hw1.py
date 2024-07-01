d18775bcd635d06d62c75f4fe136dfde6ff720d6
Faoeltiarna <moriartystreet@gmail.com>
2024-07-01 16:45:47 +0300

    commit_5

--- m3_hw1.py
+++ m3_hw1.py
@@ -3,7 +3,7 @@ import datetime
 def get_days_from_today(date):
     date_value = datetime.datetime.strptime(date, "%Y-%m-%d").date()
     current_date = datetime.datetime.today().date()
-    difference = date_obj.toordinal() - current_date.toordinal()
+    difference = date_value.toordinal() - current_date.toordinal()
     return difference
 
 
